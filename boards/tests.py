0bb586bafaba3088e237c4adf0186557e2db3930
Raihan Uddin <blackedition.me@gmail.com>
2017-12-01 01:19:03 +0600

    testing the home page & url

--- boards/tests.py
+++ boards/tests.py
@@ -1,3 +1,17 @@
+from django.core.urlresolvers import reverse
+from django.urls import resolve
 from django.test import TestCase
 
+from .views import home
+
+
 # Create your tests here.
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEqual(view.func, home)
