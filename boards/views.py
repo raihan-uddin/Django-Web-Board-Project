8e342c59a906e363f7e775155e988c2a2b49ef63
Raihan Uddin <blackedition.me@gmail.com>
2017-12-01 00:25:33 +0600

    first view from model

--- boards/views.py
+++ boards/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Board
+
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    boards = Board.objects.all()
+    boards_names = list()
+
+    for board in boards:
+        boards_names.append(board.name)
+
+    response_html = '<br>'.join(boards_names)
+    return HttpResponse(response_html)
