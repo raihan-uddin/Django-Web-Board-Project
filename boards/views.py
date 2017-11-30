7a02cb65eea953240a1a5a15d14f18a9d9eb8b7a
Raihan Uddin <blackedition.me@gmail.com>
2017-12-01 00:35:55 +0600

    Django Template Engine Setup

--- boards/views.py
+++ boards/views.py
@@ -8,10 +8,4 @@ from .models import Board
 
 def home(request):
     boards = Board.objects.all()
-    boards_names = list()
-
-    for board in boards:
-        boards_names.append(board.name)
-
-    response_html = '<br>'.join(boards_names)
-    return HttpResponse(response_html)
+    return render(request, 'home.html', {'boards': boards})
