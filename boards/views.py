0a9db0e1f440a9a40ca86c81bebacabd9d8b38b3
Raihan Uddin <blackedition.me@gmail.com>
2017-12-01 21:58:00 +0600

    refactor board_topics for shortcurt return of 404

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 from .models import Board
 
@@ -22,8 +21,5 @@ def about_company(request):
 
 
 def board_topics(request, pk):
-    try:
-        board = Board.objects.get(pk=pk)
-    except Board.DoesNotExist:
-        raise Http404
+    board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
