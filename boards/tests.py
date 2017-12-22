8b87d14ca9390127e46afad05c0cedfbc7498b18
Raihan Uddin <blackedition.me@gmail.com>
2017-12-22 10:08:02 +0600

    unit test for new topic

--- boards/tests.py
+++ boards/tests.py
@@ -3,7 +3,7 @@ from django.urls import resolve
 from django.test import TestCase
 
 from boards.models import Board
-from .views import home, board_topics
+from .views import home, board_topics, new_topic
 
 
 # Create your tests here.
@@ -35,7 +35,7 @@ class BoardTopicsTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_board_topics_view_not_found_status_code(self):
-        url = reverse('board_topics', kwargs={'pk':99})
+        url = reverse('board_topics', kwargs={'pk': 99})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
@@ -43,3 +43,27 @@ class BoardTopicsTests(TestCase):
         view = resolve('/boards/1/')
         self.assertEqual(view.func, board_topics)
 
+
+class NewTopicTest(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django Board.')
+
+    def test_new_topic_view_success_status_code(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_new_topic_view_not_found_status_code(self):
+        url = reverse('new_topic', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_new_topic_url_resolves_new_topic_view(self):
+        view = resolve('/boards/1/new/')
+        self.assertEquals(view.func, new_topic)
+
+    def test_new_topic_view_contains_link_back_to_boards_topics_view(self):
+        new_topic_url = reverse('new_topic', kwargs={'pk': 1})
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(new_topic_url)
+        self.assertContains(response, 'href="{0}"'.format(board_topics_url))
