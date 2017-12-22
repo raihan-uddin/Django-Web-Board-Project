9ed5dd02be1e985b31c33f89e7bd08f2f318ef87
Raihan Uddin <blackedition.me@gmail.com>
2017-12-22 09:54:56 +0600

    new topic view

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,6 +21,7 @@ from boards import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
 
     # simple URL routing
     # Simple URL structure
