674faeb9583b091bf0e606bc4c366fe7f51fd5e1
Raihan Uddin <blackedition.me@gmail.com>
2017-12-25 11:07:01 +0600

    submit new post & show create new post & show

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404, redirect
 
-from .models import Board
+from .models import Board, Topic, Post
 
 
 # Create your views here.
@@ -27,4 +28,23 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first() # TODO: get the currently Logged in user
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            starter=user
+        )
+
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+        return redirect('board_topics', pk=board.pk) # TODO: redirect to the created topic page
+
     return render(request, 'new_topic.html', {'board': board})
