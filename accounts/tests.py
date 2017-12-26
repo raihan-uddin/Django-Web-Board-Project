277bb007c1868ba9cdb6bfda19572ead47d18ffa
Raihan Uddin <blackedition.me@gmail.com>
2017-12-26 09:19:19 +0600

    Invalid signup tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -28,6 +28,7 @@ class SignUpTests(TestCase):
         form = self.response.context.get('form')
         self.assertIsInstance(form, UserCreationForm)
 
+
 class SuccessfulSignUpTests(TestCase):
     def setUp(self):
         url = reverse('signup')
@@ -57,3 +58,22 @@ class SuccessfulSignUpTests(TestCase):
         response = self.client.get(self.home_url)
         user = response.context.get('user')
         self.assertTrue(user.is_authenticated)
+
+
+class InvalidSignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.post(url, {})
+
+    def test_signup_status_code(self):
+        """
+        An invalid form submission should return to the same page
+        """
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_form_errors(self):
+        form = self.response.context.get('form')
+        self.assertTrue(form.errors)
+
+    def test_dont_create_user(self):
+        self.assertFalse(User.objects.exists())
