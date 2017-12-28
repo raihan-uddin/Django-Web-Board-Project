7bd05d6437db02f95340bc79dcdf2ac2fbc84480
Raihan Uddin <blackedition.me@gmail.com>
2017-12-28 09:49:59 +0600

    user signup

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 from accounts import views as accounts_views
 from boards import views
@@ -24,5 +25,6 @@ urlpatterns = [
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
