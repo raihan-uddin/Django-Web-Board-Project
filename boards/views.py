c5e18d4757f1bb8014b292fca322dc581765579c
Raihan Uddin <blackedition.me@gmail.com>
2017-12-30 23:38:39 +0600

    protecting views

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
 from boards.forms import NewTopicForm
@@ -16,7 +17,7 @@ def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
 
-
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
     user = User.objects.first()  # TODO: get the currently Logged in user
