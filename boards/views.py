455e0474b95d6e9be0f87391c4cc1e39b9371dbb
Raihan Uddin <blackedition.me@gmail.com>
2018-01-24 11:29:28 +0600

    pagination & fixed some problems

--- boards/views.py
+++ boards/views.py
@@ -39,11 +39,15 @@ class PostListView(ListView):
     model = Post
     context_object_name = 'posts'
     template_name = 'topic_posts.html'
-    paginate_by = 10
+    paginate_by = 20
 
     def get_context_data(self, **kwargs):
-        self.topic.views += 1
-        self.topic.save()
+        session_key = 'viewed_topic_{}'.format(self.topic.pk)
+        if not self.request.session.get(session_key, False):
+            self.topic.views += 1
+            self.topic.save()
+            self.request.session[session_key] = True
+
         kwargs['topic'] = self.topic
         return super().get_context_data(**kwargs)
 
@@ -85,6 +89,10 @@ def reply_topic(request, pk, topic_pk):
             post.topic = topic
             post.created_by = request.user
             post.save()
+
+            topic.last_update = timezone.now()
+            topic.save()
+
             return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
     else:
         form = PostForm
